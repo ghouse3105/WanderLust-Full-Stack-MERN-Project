be6488a692ad121e85a5f283d2fc636eb212ad59
ghouse3105 <mohmadghouse436@gmail.com>
2025-12-16 12:53:20 +0530

    Remove unnecessary blank line in error handler

--- app.js
+++ app.js
@@ -145,7 +145,6 @@ app.all("*", (req, res, next) => {
 
 
 
-
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).send(message);
